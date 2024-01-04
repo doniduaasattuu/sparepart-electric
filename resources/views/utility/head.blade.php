95d453a9d6bdcb8ca883a966962db6122172f9bb
doniduaasattuu <doni.duaasattuu@gmail.com>
2024-01-04 10:36:29 +0700

    create sql for database master sparepart electric

--- resources/views/utility/head.blade.php
+++ resources/views/utility/head.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="/storage/assets/icons.png" type="image/x-icon">
+    <link rel="icon" href="/storage/assets/icon.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
@@ -10,7 +10,7 @@
     <!-- ROBOTO MONO FONT -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
     <title>
         {{ $title }}
--- resources/views/utility/navbar.blade.php
+++ resources/views/utility/navbar.blade.php
@@ -21,8 +21,8 @@
                 <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
                 <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z" />
             </svg> -->
-            <img src="/storage/assets/icons.png" alt="EI2" width="30" height="30" class="d-inline-block align-text-top">
-            <span class="ms-1">Sparepart Information System</span>
+            <img src="/storage/assets/icon.png" alt="EI2" width="30" height="30" class="d-inline-block align-text-top">
+            <span class="ms-1" style="font-family: 'Roboto', sans-serif; font-weight: 700"><i>Sparepart Information System</i></span>
         </a>
         <button class="navbar-dark navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
